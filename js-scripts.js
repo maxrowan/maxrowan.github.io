10eb9da37addb7b954b7c2464f1bb5a7f9f3f23a
Max Rowan <maxrow3@gmail.com>
2018-06-26 14:34:37 -0400

    projects section updated
    contact section added
    
    social media icons added

--- js-scripts.js
+++ js-scripts.js
@@ -1,33 +1,77 @@
-$( document ).ready( function() {
+$( document ).ready( function () {
 
 	// slides navbar down when user scrolls
-	$( document ).one( 'scroll', function() {
+	$( document ).one( 'scroll', function () {
 		$navbar = $( '#navbar' );
-		$navbar.slideUp( 0, function() {
-			$navbar.removeClass( 'hidden' ) .slideDown( 'fast' );
+		$navbar.slideUp( 0, function () {
+			$navbar.removeClass( 'hidden' ).slideDown( 'fast' );
+		} );
+	} );
+
+
+	// navbar or brand link (scroll) to sections
+	$( ".nav-link[href^='#']" ).click( function ( e ) {
+		e.preventDefault();
+
+		$( "li.active" ).removeClass( 'active' );
+
+		$( this ).parent().addClass( 'active' );
+
+		let pos = $( $( this ).attr( "href" ) ).offset().top - 88;
+
+		$( 'body, html' ).animate( {
+			scrollTop: pos
+		} );
+	} );
+	$( "#brand" ).click( function ( e ) {
+		e.preventDefault();
+
+		$( 'li.active' ).removeClass( 'active' );
+
+		$( '#home-nav-li' ).addClass( 'active' );
+
+		$( 'body, html' ).animate( {
+			scrollTop: 0
 		});
-	});
+	} );
 
-
-
-	// smooth scroll navbar
+	// highlight nav item by window position
 
 
 	// project hover - links to carousel
 	let $carousel = $( '.carousel' );
 	$carousel.carousel( 'pause' );
 
-	$( '#weather-title' ).mouseenter( function() {
+	$( '#weather-title' ).mouseenter( function () {
 		$carousel.carousel( 0 );
 		$carousel.carousel( 'pause' );
-	});
-	$( '#android-title' ).mouseenter( function() {
+	} );
+	$( '#android-title' ).mouseenter( function () {
 		$carousel.carousel( 1 );
 		$carousel.carousel( 'pause' );
-	});
-	$( '#egams-title' ).mouseenter( function() {
+	} );
+	$( '#egams-title' ).mouseenter( function () {
 		$carousel.carousel( 2 );
 		$carousel.carousel( 'pause' );
 	} );
-});
+
+
+	// contact
+	$( '#submit-btn' ).click( function () {
+		let email = $( '#email' ).val();
+		let subject = $( '#subject' ).val();
+		let message = $( '#message' ).val();
+
+
+		// test code
+		let submitPayload = {
+			email: email,
+			subject: subject,
+			message: message
+		};
+
+		console.log( submitPayload );
+	} );
+
+} );
 
